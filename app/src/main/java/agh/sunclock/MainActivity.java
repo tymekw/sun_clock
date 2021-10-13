93c7cef89d8f920b2aab6f9637b93fb9eed696fd
tymekw <tymwid22@gmail.com>
2021-10-13 16:48:20 +0200

    initial commit

---
+++ app/src/main/java/agh/sunclock/MainActivity.java
@@ -0,0 +1,14 @@
+package agh.sunclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
