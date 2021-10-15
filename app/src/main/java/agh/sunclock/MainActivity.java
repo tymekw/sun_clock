43fc6570a93d19412815f0252ec2ae43f60702b8
tymekw <tymwid22@gmail.com>
2021-10-15 17:27:58 +0200

    get phone orientation

--- app/src/main/java/agh/sunclock/MainActivity.java
+++ app/src/main/java/agh/sunclock/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -68,5 +72,72 @@ public class MainActivity extends AppCompatActivity {
                 setLabelGPS("LNG: " + String.valueOf(location.getLongitude()) + " \nLAT: " + String.valueOf(latitude));
             }
         });
+    }
+
+
+    // new code that read data about smartphones tilt around each axis
+    // floatOrientation -> matrix containing this data
+    // [0] -> z axis (North = 0)
+    // [1] -> x axis (Camera faces sky/ground) tilt upper and  bottom edge
+    // [2] -> y axis tilt left or right edge
+
+    private SensorManager sensorManager;
+    private Sensor sensorAccelerometer;
+    private Sensor sensorMagneticField;
+    private float[] floatGravity = new float[3];
+    private float[] floatGeoMagnetic = new float[3];
+
+    private float[] floatOrientation = new float[3];
+    private float[] floatRotationMatrix = new float[9];
+
+
+    public void onSensorClick(String text) {
+        Button button = (Button) findViewById(R.id.orientation);
+        button.setOnClickListener((e -> ((TextView) findViewById(R.id.latText)).setText(text)));
+    }
+
+    public void SensorManager(View view){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        SensorEventListener sensorEventListenerAccelrometer = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                floatGravity = event.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+                onSensorClick("ok");
+                System.out.println("N/S: " + floatOrientation[0] +
+                        "\n screen up/down: " + floatOrientation[1] +
+                        "\n screen left/right: " + floatOrientation[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+
+        SensorEventListener sensorEventListenerMagneticField = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                floatGeoMagnetic = event.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+                onSensorClick("ok1");
+                System.out.println("Magnetic N/S: " + floatOrientation[0] +
+                        "\n Magnetic screen up/down: " + floatOrientation[1] +
+                        "\n Magnetic screen left/right: " + floatOrientation[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+        sensorManager.registerListener(sensorEventListenerAccelrometer, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListenerMagneticField, sensorMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
